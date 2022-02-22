f70316a520624012a054d037f1c4434679c730d5
Nicault <92713918+Nicault@users.noreply.github.com>
2022-02-22 09:17:12 +0100

    corrections decalages + double liste

--- scripts/sort.js
+++ scripts/sort.js
@@ -1,123 +1,4 @@
 
-
-
-
-
-
-
-
-
-for (let i = 0 ; i < arrayOfFiltersToDisplay.length ; i++) { // si il y a des element dans les differents tableaux d'ing app ou us, on les affiche et on masque le message d'erreur
-    if (arrayOfFiltersToDisplay[i].length > 0) {
-        createListOfCheckboxes(arrayOfFiltersToDisplay[i], i)
-        messageListe[i].classList.remove("block")
-        
-    } else { // si non on affiche le message d'erreur
-        messageListe[i].classList.add("block")
-    }
-}
-
-
-
-
-// fonction qui permet d'afficher tous les elements (aucun filtre)
-
-// function displayAllElements() {
-//     console.log("all")
-
-//     // on efface tout 
-//     recipesSection.innerHTML = "" //la section recettes
-//     for (let i = 0 ; i < listOfCheckboxes.length ; i++) { // et les tableaux de filtres
-//         listOfCheckboxes[i].innerHTML = ""
-//     }    
-//     // listOfIngAppUs = [] //et on réinitialise la liste des filtres qu'on vient d'effacer car on la rerempli à chaque fois
-//     clickableIng = []
-//     clickableApp = []
-//     clickableUs = []
-
-//     listOfIngAppUs = [clickableIng, clickableApp, clickableUs]
-//    //  on affiche tout
-//    displayData(recipes) //on display les données initiales des recettes
-
-//    arrayOfFiltersToDisplay = arrayOfOrderedFilters 
-//    for (let i = 0 ; i < inputButtons.length ; i++) { // et des filtres
-//         if (inputButtons[i].value) {
-//             createListOfCheckboxes(filterInputs(i), i)
-//         } else {
-//             createListOfCheckboxes(arrayOfFiltersToDisplay[i], i) // on crée les listes on rempli listOfIngAppUs
-//         }
-//    }
-//    createAndDeleteFilters(0, triElement.length)
-//  // on active la création des tags sur les nouvelles listes
-//  displayOnClick()
-// }
-
-
-
-
-
-// function displayFilteredElements() {
-//     console.log("fil")
-//     // on efface tout
-//     recipesSection.innerHTML = ""
-//     for (let i = 0 ; i < listOfCheckboxes.length ; i++) {
-//         listOfCheckboxes[i].innerHTML = ""
-//     } 
- 
-//      clickableIng = []// important de vider les tableaux
-//      clickableApp = []
-//      clickableUs = []
- 
-//      listOfIngAppUs = [clickableIng, clickableApp, clickableUs]
-   
-//     // on filtre les tableaux de tags par rapport à recipesToDisplay : on ne garde que les ingredients, appareils et ustensiles présents dans la liste
-//     applianceToDisplay = filterAppliances(recipesToDisplay)
-//     ingredientsToDisplay = filterIngredients(recipesToDisplay)
-//     ustensilesToDisplay = filterUstensils(recipesToDisplay)
- 
-//     // on met à jour le tableau avec les nouvelles listes
-//     arrayOfFiltersToDisplay = [ingredientsToDisplay, applianceToDisplay, ustensilesToDisplay]
-    
-    
- 
-//     // on affiche les recettes
-//     if (recipesToDisplay.length > 0) { // si il y a des element dans le tableau recipesToDisplay, on les affiche
-//         displayData(recipesToDisplay)
-//     } else { //si non on affiche le message d'erreur
-//         let message = document.createElement("div")
-//         message.classList.add("message")
-//         message.textContent = "Aucune recette ne correspond à votre recherche... Vous pouvez chercher 'tarte aux pommes', 'poisson', etc."
-//         recipesSection.appendChild(message)
-//     }
- 
-//     // on créé les listes de filtres
-//     for (let i = 0 ; i < inputButtons.length ; i++) { // si il y a des element dans les differents tableaux d'ing app ou us, on les affiche et on masque le message d'erreur
-//         if (inputButtons[i].value) {
-//             // console.log("avant" + arrayOfFiltersToDisplay[i])
-//             arrayOfFiltersToDisplay[i] = filterInputs(i)
-//             // console.log("apres" + arrayOfFiltersToDisplay[i])
-            
-//         }       
-//     }
-
-//     arrayOfFiltersToDisplay = [ingredientsToDisplay, applianceToDisplay, ustensilesToDisplay]
-
-//     for (let i = 0 ; i < inputButtons.length ; i++) { // et des filtres
-//         createListOfCheckboxes(arrayOfFiltersToDisplay[i], i) // on crée les listes on rempli listOfIngAppUs
-//     }
-   
- 
-//     createAndDeleteFilters(0, triElement.length)
-//   // on active la création des tags sur les nouvelles listes
-//     displayOnClick() // et on reactive la fonction qui affiche a click car les elemnts ont été supprimés et recréés
-
-//  } 
-
-
-
-
-
-
 function displayFilteredElements() {
     // on efface tout
     recipesSection.innerHTML = ""
@@ -161,6 +42,14 @@ function displayFilteredElements() {
                 createListOfCheckboxes(arrayOfFiltersToDisplay[i], i) // on crée les listes on rempli listOfIngAppUs
         }
     }
+
+    for (let i = 0 ; i < divOfCheckboxes.length ; i++) { //on ajoute ou on eleve le message d'erreur si pas de resultat dans la liste
+        if (listOfIngAppUs[i].length == 0) {
+            messageListe[i].classList.add("block")
+        } else {
+            messageListe[i].classList.remove("block")   
+        }
+    }         
     
     createAndDeleteFilters(0, triElement.length)   
     displayOnClick() 
@@ -179,15 +68,7 @@ function displayFilteredElements() {
 function displayOnSearchbar() {
     searchBar.addEventListener("input", function(e) { // à l'input
         e.preventDefault()
-        displayFilteredElements()
-        
-        for (let i = 0 ; i < divOfCheckboxes.length ; i++) {
-            if (listOfIngAppUs[i].length == 0) {
-                messageListe[i].classList.add("block")
-            } else {
-                messageListe[i].classList.remove("block")   
-            }
-        }           
+        displayFilteredElements()         
         // displayOnClick()
 
     })
@@ -210,11 +91,6 @@ function displayOnClick() {
                 displayFilteredElements()
             })
         })
-        if (listOfIngAppUs[i].length == 0) {
-            messageListe[i].classList.add("block")
-        } else {
-            messageListe[i].classList.remove("block")   
-        }
     }
 
 }
@@ -234,7 +110,6 @@ for (let i = 0 ; i < inputButtons.length ; i++ ) { // à la saisie dans un des i
         e.preventDefault()
         console.log("input")
         listOfCheckboxes[i].innerHTML = ""
-        // listOfIngAppUs = [] ça ne marche pas car si on efface tout ca ne recréera que les elemenet du tableau actif
         if (inputButtons[i].parentElement.classList.contains("blue")) {
             clickableIng = []
         } else if (inputButtons[i].parentElement.classList.contains("green")) {
@@ -242,9 +117,7 @@ for (let i = 0 ; i < inputButtons.length ; i++ ) { // à la saisie dans un des i
         } else {
             clickableUs = []
         }
-        // console.log(listOfIngAppUs)
         listOfIngAppUs = [clickableIng, clickableApp, clickableUs]
-        // console.log(listOfIngAppUs)
         
         let elementToDisplay = filterInputs(i)
         if (elementToDisplay.length) { 
